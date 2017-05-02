2f2789ae3a4d57ac7646a4a15130064cf1b6e95e
陈治谋 <513500085@qq.com>
2017-05-02 10:36:16 +0800

    - 0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # bank-info
+
+通过银行卡号前六位获取银行名称
+
+
+## Install
+
+```groovy
+repositeries{
+  maven { url "http://maven.mjtown.cn/"}
+}
+dependencies{
+  compile "name.zeno:bank-info:0.0.1"
+}
+```
+
+## Usage
+
+```
+String bankInfo = BankInfo.getNameOfBank("356827");
+// bankInfo: 上海银行·申卡贷记卡
+```
---
+++ app/src/main/java/name/zeno/bankinfo/demo/MainActivity.java
@@ -0,0 +1,15 @@
+package name.zeno.bankinfo.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
